8b613b0379fc50ad42edb8a1fc95e33f16e2c5ff
corbin Boughen <50217007+theBough@users.noreply.github.com>
2023-05-18 10:27:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var rndNum;
 function generateRndNum(){
   //this will make a random number
   //for the play to guess
-  rndNum = Math.random()
+  //This will make a random number between 1-500
+  rndNum = Math.random()*500
+  rndNum = Math.ceil(rndNum)
   document.getElementById("title").innerHTML = rndNum
 }
